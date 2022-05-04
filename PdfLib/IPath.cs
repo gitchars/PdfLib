4aa044b715b05e798dd682228a13fd34248ac251
Charlie Nunez <nunezr@gmail.com>
2022-05-04 12:19:33 -0300

    Path interface  and Operators constants class finalized

--- PdfLib/IPath.cs
+++ PdfLib/IPath.cs
@@ -9,6 +9,33 @@ namespace PdfLib
     public interface IPath
     {
         Point currenPoint { get; }
+        double lineWidth { get; set; }
+
+        void MoveTo(double x, double y);
+
+        void LineTo(double x, double y);
+
+        void CurveC(double x1, double y1, double x2, double y2, double x3, double y3);
+
+        void CurveV(double x2, double y2, double x3, double y3);
+
+        void CurveY(double x1, double y1, double x3, double y3);
         
+        void ClosePath();
+        
+        void Rectangle(double x, double y, double width, double height);
+
+
+        // --- Path-painting operators
+        string Stroke();
+
+        string ClosePathAndStroke();
+        string Fill();
+        string FillAndStroke_UsingNZWN();
+        string FillAndStroke_UsingEOR();
+
+        string CloseFillAndStroke_UsingNZWN();
+        string CloseFillAndStroke_UsingEOR();
+
     }
 }
---
+++ PdfLib/Operators.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfLib
+{
+    static class Operators
+    {
+        public const string BeginPath = "m";
+        public const string Straightline = "l";
+        public const string CurveC = "c";
+        public const string CurveV = "v";
+        public const string CurveY = "y";
+        public const string ClosePath = "h";
+        public const string Rectangle = "re";
+
+        public const string StrokePath = "S";
+        public const string CloseStrokePath = "s";
+        
+        public const string FillPath_usingNZWN = "f";
+        public const string FillPath_usingeor = "f*";
+        
+        public const string FillAndStroke_usingNZWN = "B";
+        public const string FillAndStroke_usingeor = "B*";
+
+        public const string CloseFillAndStroke_usingNZWN = "b";
+        public const string CloseFillAndStroke_usingeor = "b*";
+
+    }
+}
