ca8a9d260a9b202f1d3cc9a25a5da28f7b624191
Charlie Nunez <nunezr@gmail.com>
2022-05-04 10:35:18 -0300

    First commit

---
+++ PdfLib.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("PdfLib.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PdfLib.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b090cd93-2bc7-4df8-ab48-ca11154fcf25")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ PdfLib/IPdf.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PdfLib
+{
+        public interface IPdf
+        {
+            string headerContent { get; set; }
+            IBodyPdf BodyContent { get; set; }
+            string xRefContent { get; set; }
+            string TrailerContent { get; set; }
+
+            bool SaveFile(string filePath, string fileName);
+
+        }
+
+}
